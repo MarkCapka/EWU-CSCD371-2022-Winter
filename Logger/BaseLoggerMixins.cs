55d05500d4c0a837d60caf01243e760a880a181d
Tyler Rose <47163327+TylerRose@users.noreply.github.com>
2022-01-22 13:47:42 -0800

    BaseLoggerMixins update

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -3,27 +3,27 @@
 namespace Logger;
 public static class BaseLoggerMixins
 {
-    public static void Error(this BaseLogger logger, string message, params Object[] moreStuff)
+    public static void Error(this BaseLogger logger, string message, params Object[] arrayParameters)
     {
-        message = String.Format(message, moreStuff);
+        message = String.Format(message, arrayParameters);
         logger.Log(LogLevel.Error, message);
     }
 
-    public static void Warning(this BaseLogger logger, string message, params Object[] moreStuff)
+    public static void Warning(this BaseLogger logger, string message, params Object[] arrayParameters)
     {
-        message = String.Format(message, moreStuff);
+        message = String.Format(message, arrayParameters);
         logger.Log(LogLevel.Warning, message);
     }
 
-    public static void Information(this BaseLogger logger, string message, params Object[] moreStuff)
+    public static void Information(this BaseLogger logger, string message, params Object[] arrayParameters)
     {
-        message = String.Format(message, moreStuff);
+        message = String.Format(message, arrayParameters);
         logger.Log(LogLevel.Information, message);
     }
 
-    public static void Debug(this BaseLogger logger, string message, params Object[] moreStuff)
+    public static void Debug(this BaseLogger logger, string message, params Object[] arrayParameters)
     {
-        message = String.Format(message, moreStuff);
+        message = String.Format(message, arrayParameters);
         logger.Log(LogLevel.Debug, message);
     }
 }

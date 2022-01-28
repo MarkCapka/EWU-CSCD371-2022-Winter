b5d23c277fecff1f2aa05ecdfe29d9422b0ab0a5
Tyler Rose <47163327+TylerRose@users.noreply.github.com>
2022-01-27 22:48:00 -0800

    Fix file name substring issues

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -21,7 +21,17 @@ public class FileLogger : BaseLogger
 
     public override void Log(LogLevel logLevel, string message, [System.Runtime.CompilerServices.CallerFilePath] string loggedBy = "")
     {
-        string loggedByFileName = loggedBy.Substring(loggedBy.LastIndexOf("\\"), loggedBy.LastIndexOf("."));
+        string loggedByFileName;
+        if (string.IsNullOrEmpty(loggedBy))
+        {
+            loggedByFileName = "";
+        }
+        else
+        {
+            //use .Remove to delete everything before the file name, then use .Remove again to delete the file extension
+            loggedByFileName = loggedBy.Remove(0, loggedBy.LastIndexOf("\\"));
+            loggedByFileName = loggedByFileName.Remove(loggedByFileName.LastIndexOf("."));
+        }
         string logEvent = $"{DateTime.Now} {loggedByFileName} {logLevel}: {message}\n";
         File.AppendAllText(FilePath, logEvent);
     }

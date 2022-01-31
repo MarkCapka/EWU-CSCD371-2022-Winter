03a7427de52960fec0c23904d78c5d6827fb04b7
Tyler Rose <47163327+TylerRose@users.noreply.github.com>
2022-01-30 20:26:48 -0800

    How many Chuck Norris jokes could a JokeService chuck, if a joke service could chuck Chuck Norris?

--- CanHazFunny/CanHazFunny/JokeService.cs
+++ CanHazFunny/CanHazFunny/JokeService.cs
@@ -8,8 +8,13 @@ namespace CanHazFunny
 
         public string? GetJoke()    //TODO confirm if nullable should be addressed.
         {
-            string joke = "Links to Joke Service: Generates Joke, commented out until final result to avoid API calls..."; //TODO add  line below back and comment out this one. For sake of not making incessant calls to the API. 
-                                                                                                                           //   string joke = HttpClient.GetStringAsync("https://geek-jokes.sameerkumar.website/api").Result;
+            string joke = "Links to Joke Service: Generates Joke, commented out until final result to avoid API calls...";
+            //TODO add  line below back and comment out the one above. For sake of not making incessant calls to the API. 
+            //joke = HttpClient.GetStringAsync("https://geek-jokes.sameerkumar.website/api").Result;
+            while (joke.Contains("Chuck Norris"))
+            {
+                joke = HttpClient.GetStringAsync("https://geek-jokes.sameerkumar.website/api").Result;
+            }
 
             return joke;
         }

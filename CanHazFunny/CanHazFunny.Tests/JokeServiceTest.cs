31d90dea24dcc039557c1d91b723e4159145ce22
Tyler Rose <47163327+TylerRose@users.noreply.github.com>
2022-02-03 23:27:33 -0800

    JokeService tests

---
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace CanHazFunny.Tests;
+[TestClass]
+public class JokeServiceTest
+{
+    [TestMethod]
+    public void GetJoke_GetsAValidJokeFromAPI_Success()
+    {
+        JokeService service = new JokeService();
+        string newJoke = service.GetJoke();
+
+        Assert.IsNotNull(newJoke);
+        Assert.IsTrue(!newJoke.ToLower().Contains("chuck") & !newJoke.ToLower().Contains("norris"));
+    }
+}

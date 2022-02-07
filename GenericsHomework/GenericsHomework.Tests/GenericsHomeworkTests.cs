5c42bcac3818eb142b2bfc18aefb9a2063ae3a5d
Tyler Rose <47163327+TylerRose@users.noreply.github.com>
2022-02-06 16:45:00 -0800

    LinkedList and Tests

--- GenericsHomework/GenericsHomework.Tests/GenericsHomeworkTests.cs
+++ GenericsHomework/GenericsHomework.Tests/GenericsHomeworkTests.cs
@@ -7,7 +7,64 @@ namespace GenericsHomework.Tests
     public class GenericsHomeworkTests
     {
         [TestMethod]
-        public void NodeConstructNode_ConstructsNode_Success()
+        public void Node_ConstructsHeadNode_Success()
+        {
+            //TODO just playing around - need to confirm that these are "behaving"
+            //TODO I thin ki need to add construction
+         
+  
+            string value = "hello";
+            Head = new Node<T>(value);
+
+            node.Next = Head; //since only thing it will point to itself
+
+            Assert.AreEqual<T>(value, "hello");
+            Assert.AreEqual<T>(Head.Next, Head);
+        }
+
+        [TestMethod]
+        public void Node_ConstructsNode_Success()
+        {
+            //TODO just playing around - need to confirm that these are "behaving"
+            //TODO I thin ki need to add construction
+            
+            Node<string> head = new Node<string>(value);
+            Node<string> node1 = new();
+            Node<string> node2 = new();
+         //   Node<int> node3 = new(); //TODO consider if we can just use the generic to swap with the int mid-class. 
+            Head.Append(node1);
+
+            node1.Append(node2);
+
+
+            string value = "hello";
+            Head = new Node<T>(value);
+
+            node.Next = Head; //since only thing it will point to itself
+
+            Assert.AreEqual<string>(value, "hello");
+            Assert.AreEqual<Node<string>>(Head.Next, Head);
+        }
+
+        [TestMethod]
+        public void SimpleLinkedList_AppendsNode_Success()
+        {
+            throw new NotImplementedException();
+
+            //Assert.AreEqual<T>();
+            //TODO test for empty linkedlist
+        }
+
+        [TestMethod]
+        public void SimpleLinkedList_RemovesNode_Success()
+        {
+            throw new NotImplementedException();
+
+            //Assert.AreEqual<T>();
+        }
+
+        [TestMethod]
+        public void Node_StringsNode_Success()
         {
             throw new NotImplementedException();
 
@@ -17,23 +74,28 @@ namespace GenericsHomework.Tests
 
 
         [TestMethod]
-        public void NodeAppendNode_AppendsNode_Success()
-        {
-            throw new NotImplementedException();
-
-           //Assert.AreEqual<T>();
-        }
-
-        [TestMethod]
-        public void NodeRemoveNode_RemovesNode_Success()
+        public void SimpleLinkedList_ClearsList_Success()
         {
             throw new NotImplementedException();
 
             //Assert.AreEqual<T>();
         }
 
+
+
         [TestMethod]
-        public void NodeToString_StringsNode_Success()
+        public void SimpleLinkedList_AddsDuplicate_Fail()
+        {
+            throw new NotImplementedException();
+
+            //Assert.AreEqual<T>();
+        }
+
+
+
+        [TestMethod]
+        [ExpectedException]
+        public void SimpleLinkedList_AttemptToAddDuplicateThowsException_Fail()
         {
             throw new NotImplementedException();
 
--- GenericsHomework/GenericsHomework/SimpleLinkedList.cs
+++ GenericsHomework/GenericsHomework/SimpleLinkedList.cs
@@ -1,26 +1,60 @@
 ﻿
+using System.Collections;
 using System.Text;
 
 namespace GenericsHomework;
-internal class SimpleLinkedList<T> where T : notnull
+internal class SimpleLinkedList<T> : IEnumerable, IEnumerator<Node<T>> where T : notnull
 {
-    private Node<T>? Head { get; set; }
+    private Node<T> Head { get; set; }
 
+    object IEnumerator.Current => Head;//throw new NotImplementedException();
+
+    public Node<T> Current => Head;// throw new NotImplementedException();
+
+    public SimpleLinkedList(T value)
+    {
+        Head = new Node<T>(value);
+    }
+    public SimpleLinkedList(Node<T> head)
+    {
+        Head = head;
+    }
     public void Append(T value)
     {
-        if (Head is null)
-        {
-            Head = new Node<T>(value);
-        }
         Node<T> current = Head;
         while (!current.Next.Equals(current))
         {
             current = current.Next;
+            if (current.NodeData.Equals(value))
+            {
+                throw new ArgumentException($"NodeData with value {value} is a duplicate to another Node's data");
+            }
         }
         current.Next = new Node<T>(value);
     }
+    public bool Exists(T value)
+    {
+        Node<T> current = Head;
+        while (!current.Next.Equals(current) && !current.NodeData.Equals(value))
+        {
+            current = current.Next;
+        }
+        return current.NodeData.Equals(value);
+    }
 
-    //I think this is to establish arrow relationship between nodes. 
+
+    //TODO consider: Whether it is sufficient to only set Next to itself ❌✔
+    //Whether to set the removed items to circle back on themselves. In other words, whether to close the loop of the removed items. (Provide a test to show why this is required if it is required). ❌✔
+    //Given there is a circular list of items, provide a comment to indicate whether you need to worry about garbage collection because all the items point to each other and therefore may never be garbage collected. ❌✔
+    public void Clear()
+    {
+        Head = Current;
+        Head.Next = Head;
+
+        //if we set the End to the head (not circularly but assign it as the head) it will have nothing after it. 
+    }
+
+    //To establish arrow relationship between nodes. 
     public override string ToString()
     {
         StringBuilder outputListAsString = new StringBuilder();
@@ -39,4 +73,31 @@ internal class SimpleLinkedList<T> where T : notnull
         return outputListAsString.ToString();
     }
 
+    public IEnumerator GetEnumerator()
+    {
+        return this;
+    }
+
+    public bool MoveNext()
+    {
+        if (Current.Next.Equals(Current))
+        {
+            return false;
+        }
+        else
+        {
+            //Current = Current.Next;
+            return true;
+        }
+    }
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
 }

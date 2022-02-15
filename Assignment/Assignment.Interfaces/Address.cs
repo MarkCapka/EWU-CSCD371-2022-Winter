21cb17af93782fa789746fbfb87ed8a8f57facfa
MarkCapka <markcapka@gmail.com>
2022-02-14 16:19:49 -0800

    setting up a few classes, making constructor items nulllable for now to kick the can down the road on handling the errors .

---
+++ Assignment/Assignment.Interfaces/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Address : IAddress
+    {
+        public string? StreetAddress {get; set;}
+
+        public string? City { get; set; }
+
+        public string? State {get; set;}
+
+    public string? Zip { get; set; }
+    }
+}

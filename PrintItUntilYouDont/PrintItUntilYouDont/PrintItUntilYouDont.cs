7fa12ffb136df6d638433f7a282dc70b8bc0e27c
MarkCapka <61602557+MarkCapka@users.noreply.github.com>
2022-03-08 15:26:34 -0800

    my in class notes on multithreading

---
+++ PrintItUntilYouDont/PrintItUntilYouDont/PrintItUntilYouDont.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintItUntilYouDont;
+public class PrintItUntilYouDont
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello, World!");
+
+
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+
+        Task task = Task.Run(
+             () => Display('.', cancellationTokenSource.Token)); //issuing a single token to the display method
+            
+
+        Console.WriteLine("Enter to exit");
+        Console.ReadLine();
+        cancellationTokenSource.Cancel(); //this will cancel any available tokens withing the method. 
+        task.Wait();
+        Console.WriteLine("Exiting");
+
+    }
+
+
+//NOTE: cancellationToken gives us access to the cancellationToken, it is NOT the cancellationTokenSource itself
+    static void Display(char character, CancellationToken cancellationToken)
+    {
+        while (true)
+        {
+            Console.Write(character);
+        }
+    }
+}
+
